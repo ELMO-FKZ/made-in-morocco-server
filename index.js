43d10060212cee2c4c5209b70493ad498a95f429
EL MOKHTAR <122396078+ELMO-FKZ@users.noreply.github.com>
2024-01-10 09:51:22 +0100

    Adding vercel.json

--- index.js
+++ index.js
@@ -16,7 +16,8 @@ connectDB();
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
-app.use(cors({origin: `https://made-in-morocco.onrender.com`, credentials: true}))
+// app.use(cors({origin: 'http://localhost:5173', credentials: true}));
+app.use(cors({origin: `https://made-in-morocco.onrender.com`, credentials: true}));
 
 // APIs
 app.use("/api", shopRoutes);
